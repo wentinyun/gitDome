ab0a66bb3044bf86907b19e18320908c5d769ecf
wentingyun <150759338+wentinyun@users.noreply.github.com>
2025-12-10 15:06:04 +0800

    第一次提交

---
+++ src/main/java/com/wentingyun/GitDomeApplication.java
@@ -0,0 +1,13 @@
+package com.wentingyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wentingyun/GitDomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wentingyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
